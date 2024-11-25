f73a6ddf4313dec36630e8e13bcac04c83fa3621
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-11-25 16:58:03 +0900

    02_03完成

--- Input.cpp
+++ Input.cpp
@@ -10,12 +10,12 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 	HRESULT result;
 
 	// DirectInputのインスタンス生成
-	ComPtr<IDirectInput8>directInput = nullptr;
 	result = DirectInput8Create(
 		hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
 	assert(SUCCEEDED(result));
 
 	// キーボードデバイス生成
+	//ComPtr<IDirectInputDevice8>keyboard;
 	result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
 	assert(SUCCEEDED(result));
 
@@ -30,10 +30,30 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 
 void Input::Update()
 {
+	// 前回のキー入力を保存
+	memcpy(keyPre, key, sizeof(key));
+
 	// キーボード情報の所得開始
 	keyboard->Acquire();
 
 	// 全キーの入力状態を取得する
-	BYTE key[256] = {};
+	//BYTE key[256] = {};
 	keyboard->GetDeviceState(sizeof(key), key);
+}
+
+bool Input::PushKey(BYTE keyNumber)
+{
+	// 指定キーを押していればtrueを返す
+	if (key[keyNumber]) {
+		return true;
+	}
+
+	// そうでなければfalseを返す
+	return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber)
+{
+	// 今フレームで押されていて、前フレームでは押されていない場合にtrueを返す
+	return key[keyNumber] && !keyPre[keyNumber];
 }
