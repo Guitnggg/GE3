ebcc3fe600555effb9e02771ea93cc61834044e7
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-11-25 16:56:42 +0900

    a

--- Input.h
+++ Input.h
@@ -1,59 +1,45 @@
-#include "Input.h"
+#pragma once
 
-#include <cassert>
+#include <Windows.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
+#include <dinput.h>
 
-#pragma comment(lib,"dinput8.lib")
-#pragma comment(lib,"dxguid.lib")
+/// <summary>
+/// 入力
+/// </summary>
 
-void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
+class Input
 {
-	HRESULT result;
+public: // メンバ関数
 
-	// DirectInputのインスタンス生成
-	result = DirectInput8Create(
-		hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
-	assert(SUCCEEDED(result));
+	// namespace省略
+	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
-	// キーボードデバイス生成
-	//ComPtr<IDirectInputDevice8>keyboard;
-	result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
-	assert(SUCCEEDED(result));
+	// 初期化
+	void Initialize(HINSTANCE hInstance, HWND hwnd);
 
-	// 入力データ形式のセット
-	result = keyboard->SetDataFormat(&c_dfDIKeyboard);
-	assert(SUCCEEDED(result));
+	// 更新
+	void Update();
 
-	// 排他制御レベルのセット
-	result = keyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
-	assert(SUCCEEDED(result));
-}
+	// 押されているか
+	bool PushKey(BYTE keyNumber);
 
-void Input::Update()
-{
-	// 前回のキー入力を保存
-	memcpy(keyPre, key, sizeof(key));
+	// トリガー
+	bool TriggerKey(BYTE keyNumber);
 
-	// キーボード情報の所得開始
-	keyboard->Acquire();
+private: // メンバ変数
 
-	// 全キーの入力状態を取得する
-	//BYTE key[256] = {};
-	keyboard->GetDeviceState(sizeof(key), key);
-}
+	// キーボードのデバイス
+	ComPtr<IDirectInputDevice8> keyboard;
 
-bool Input::PushKey(BYTE keyNumber)
-{
-	// 指定キーを押していればtrueを返す
-	if (key[keyNumber]) {
-		return true;
-	}
+	// DirectInputのインスタンス
+	ComPtr<IDirectInput8>directInput;
 
-	// そうでなければfalseを返す
-	return false;
-}
+	// 全キーの状態
+	BYTE key[256] = {};
 
-bool Input::TriggerKey(BYTE keyNumber)
-{
-	// 今フレームで押されていて、前フレームでは押されていない場合にtrueを返す
-	return key[keyNumber] && !keyPre[keyNumber];
-}
+	// 前回の全キーの状態
+	BYTE keyPre[256] = {};
+};
+
