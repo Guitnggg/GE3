2ef189c2141ebea81215c1526122ae001a84a321
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-12-02 11:34:43 +0900

    03_02完成

--- WinApp.h
+++ WinApp.h
@@ -1,11 +1,23 @@
 #pragma once
+
+#include <Windows.h>
+
+/// <summary>
+/// WindowsAPI
+/// </summary>
+
 class WinApp
 {
 public:
 
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+public:
+
+	// 初期化
 	void Initialize();
 
+	// 更新
 	void Update();
-
 };
 
