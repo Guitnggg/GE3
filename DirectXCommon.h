c2d567f7c2080815df15641a71c82b0051ca3e5f
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-12-02 14:44:54 +0900

    DirectXCommonクラス追加

---
+++ DirectXCommon.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+
+/// <summary>
+/// DirectX基盤
+/// </summary>
+
+class DirectXCommon
+{
+	//====================
+	// メンバ関数
+	//====================
+
+	// 初期化
+	void Initialize();
+};
+
